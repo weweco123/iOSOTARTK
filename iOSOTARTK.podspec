8a39a02c00c633bdc7a5a79a005782b93b4923db
VV <w.wei@ute-tech.com.cn>
2020-10-20 12:03:51 +0800

    小修改podspec

--- iOSOTARTK.podspec
+++ iOSOTARTK.podspec
@@ -9,5 +9,12 @@ s.platform     = :ios, '9.0'
 s.source       = {:git => 'https://github.com/weweco123/iOSOTARTK.git', :tag => s.version}
 s.pod_target_xcconfig = {'VALID_ARCHS' => 'armv7 arm64'}
 s.vendored_frameworks = "Framework/RTKLEFoundation.framework","Framework/RTKOTASDK.framework"
+s.xcconfig = { 
+    'USER_HEADER_SEARCH_PATHS' => [
+        '$(inherited)',
+        '${PODS_ROOT}/Framework/RTKLEFoundation.framework/Headers',
+        '${PODS_ROOT}/Framework/RTKOTASDK.framework/Headers'
+    ] 
+}
 s.requires_arc = true
 end
--- iOSOTARTK/ViewController.m
+++ iOSOTARTK/ViewController.m
@@ -15,9 +15,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    self.view.backgroundColor = [UIColor yellowColor];
     
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    UILabel *label = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 150)];
+    label.textColor = [UIColor blackColor];
+    label.center = CGPointMake(self.view.frame.size.width*0.5, self.view.frame.size.height*0.5);
+    label.text = @"Nice";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.font = [UIFont boldSystemFontOfSize:150];
+    [self.view addSubview:label];
 }
 
 
